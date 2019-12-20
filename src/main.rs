2c21865f26ac1adb10b9f058e9587fd7c0d0d99a
Jon Boyce <jsboyce@sep.com>
2019-12-20 16:02:27 -0500

    Take zip code.

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,23 @@ extern crate reqwest;
 extern crate select;
 use select::document::Document;
 use select::predicate::{Class};
+use std::env;
 
 fn main() {
-  let body: &str = &reqwest::get("https://forecast.weather.gov/MapClick.php?site=IND&textField1=39.9693&textField2=-86.1095&e=0").unwrap().text().unwrap();
-  let document = Document::from(body);
-  println!("{} {} | {}",
-    document.find(Class("myforecast-current")).next().unwrap().text(),
-    document.find(Class("myforecast-current-lrg")).next().unwrap().text(),
-    document.find(Class("forecast-text")).next().unwrap().text());
+  let args: Vec<String> = env::args().collect();
+  let zip_bytes = include_bytes!("2019_Gaz_zcta_national.txt");
+  let zip_string = String::from_utf8_lossy(zip_bytes);
+  let zip_data = zip_string.split("\n");
+  for line in zip_data {
+    if line.starts_with(&args[1]) {
+      let chunks: Vec<&str> = line.split_whitespace().collect();
+      let body: &str = &reqwest::get(&format!("https://forecast.weather.gov/MapClick.php?lat={}&lon={}", chunks[5], chunks[6])[..]).unwrap().text().unwrap();
+      let document = Document::from(body);
+      println!("{} {} | {}",
+        document.find(Class("myforecast-current")).next().unwrap().text(),
+        document.find(Class("myforecast-current-lrg")).next().unwrap().text(),
+        document.find(Class("forecast-text")).next().unwrap().text());
+      break;
+    }
+  }
 }
